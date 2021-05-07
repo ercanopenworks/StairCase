a40ee345811b5770d7903b84b87ba5d2142b042a
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-05-07 06:34:28 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Staircase
+{
+    class Program
+    {
+        public static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                //shortest way
+                Console.WriteLine(new String('#', i).PadLeft(n, ' '));
+
+                //long way
+                //string str = new String(' ', n - i);
+                //string star = new string('#', i);
+
+                //Console.WriteLine(str + star);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            staircase(n);
+        }
+    }
+}
